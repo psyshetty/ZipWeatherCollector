1b428dbc96daa50533b82c7cb66fe0f7ba2d8280
Prashant Shetty <psshetty@gmail.com>
2016-12-28 20:39:26 -0600

    got rid of an intermediate RDD

--- src/main/java/com/phunware/interviews/weather/collector/SparkWeatherDataCollector.java
+++ src/main/java/com/phunware/interviews/weather/collector/SparkWeatherDataCollector.java
@@ -24,9 +24,8 @@ public class SparkWeatherDataCollector implements Serializable {
   public void collect(JavaSparkContext sc, final Properties config, String inputFileName) {
     JavaRDD<String> file = sc.textFile(inputFileName);
     System.out.println("Input file: " + inputFileName);
-    JavaPairRDD<Integer, String> zips = file.mapToPair(
-        new PairFunction<String, Integer, String>() {
-          public Tuple2<Integer, String> call(String line) throws Exception {
+    file.foreach(new VoidFunction<String>(){
+      public void call(String line) throws Exception {
             String[] parts = line.split(",");
             System.out.println("Line: " + line);
             Integer zipcode = null;
@@ -35,14 +34,7 @@ public class SparkWeatherDataCollector implements Serializable {
             } catch (Exception ex) {
               throw new Exception("Invalid zipcode: " + zipcode);
             }
-            Zip zip = new Zip(zipcode, parts[1], parts[2]);
-            return new Tuple2(zipcode, zip);
-          }
-        });
-    zips.foreach(new VoidFunction<Tuple2<Integer, String>>() {
-      public void call(Tuple2<Integer, String> integerStringTuple2) throws Exception {
-        Integer zipcode = integerStringTuple2._1;
-        Weather weather = YahooWeatherService.getWeather(integerStringTuple2._1, config);
+        Weather weather = YahooWeatherService.getWeather(zipcode, config);
         if (weather != null) {
           String weatherJSON = toJSON(weather);
           System.out.println("zip : " + zipcode + ", weather json: " + weatherJSON);
